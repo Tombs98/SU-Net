5010b94b901d1c8568f0f88d2989426d1d4b96cb
Tombs <53521957+Tombs98@users.noreply.github.com>
2023-03-28 17:15:05 +0800

    Add files via upload

---
+++ model/su_net.py
@@ -0,0 +1,188 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from model.arch_util import LayerNorm2d
+
+class SimpleGate(nn.Module):
+    def forward(self, x):
+        x1, x2 = x.chunk(2, dim=1)
+        return x1 * x2
+
+
+class RBlock(nn.Module):
+    def __init__(self, c, DW_Expand=2, FFN_Expand=2, drop_out_rate=0.):
+        super().__init__()
+        dw_channel = c * DW_Expand
+        self.conv1 = nn.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv2 = nn.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,
+                               bias=True)
+        self.conv3 = nn.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+           
+
+        # SimpleGate
+        self.sg = SimpleGate()
+
+        self.norm1 = LayerNorm2d(c)
+
+
+    def forward(self, inp):
+        x = inp
+        x = self.norm1(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.sg(x)
+        x = self.conv3(x)
+        return x + inp
+
+class DRBlock(nn.Module):
+    def __init__(self, c, number):
+        super().__init__()
+        self.rb = nn.ModuleList()
+        for num in range(number):
+            self.rb.append(
+                nn.Sequential(
+                    *[RBlock(c) for _ in range(num)]
+                )
+            )
+    def forward(self, inp):
+        temp = []
+        layer_input = inp
+        res = inp
+        for Rb in self.rb:
+            res = Rb(res)
+        return res + inp
+        '''
+        layer_output = torch.zeros_like(layer_input)
+        for Rb in self.rb:
+            layer_input = layer_input + layer_output
+            layer_output = Rb(layer_input)
+            temp.append(layer_output)
+        for i in temp:
+            res = res + i 
+        return res
+        '''
+class MHSAB(nn.Module):
+    def __init__(self, n_dims, width=16, height=16, heads=4):
+        super(MHSAB, self).__init__()
+        self.heads = heads
+
+        self.query = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+        self.key = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+        self.value = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+
+        self.rel_h = nn.Parameter(torch.randn([1, heads, n_dims // heads, 1, height]), requires_grad=True)
+        self.rel_w = nn.Parameter(torch.randn([1, heads, n_dims // heads, width, 1]), requires_grad=True)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        n_batch, C, width, height = x.size()
+        q = self.query(x).view(n_batch, self.heads, C // self.heads, -1)
+        k = self.key(x).view(n_batch, self.heads, C // self.heads, -1)
+        v = self.value(x).view(n_batch, self.heads, C // self.heads, -1)
+
+        content_content = torch.matmul(q.permute(0, 1, 3, 2), k)
+
+        content_position = (self.rel_h + self.rel_w).view(1, self.heads, C // self.heads, -1).permute(0, 1, 3, 2)
+        content_position = torch.matmul(content_position, q)
+
+        energy = content_content + content_position
+        attention = self.softmax(energy)
+
+        out = torch.matmul(v, attention.permute(0, 1, 3, 2))
+        out = out.view(n_batch, C, width, height)
+
+        return out
+
+class SU_Net(nn.Module):
+
+    def __init__(self, img_channel=3, class_number=1, width=64, middle_blk_num=1, enc_blk_nums=[2,2,2,2], dec_blk_nums=[2,2,2,2], drenum=2, drdnum=2):
+        super().__init__()
+
+        self.intro = nn.Conv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True)
+        self.ending = nn.Conv2d(in_channels=width, out_channels=class_number, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True)
+        self.lin = nn.Linear(256*256*3, class_number)
+
+        self.encoders = nn.ModuleList()
+        self.decoders = nn.ModuleList()
+        self.middle_blks = nn.ModuleList()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        chan = width
+        for num in enc_blk_nums:
+            self.encoders.append(
+                nn.Sequential(
+                    *[DRBlock(chan, drenum) for _ in range(num)]
+                )
+            )
+            self.downs.append(
+                nn.Conv2d(chan, 2*chan, 2, 2)
+            )
+            chan = chan * 2
+
+        self.middle_blks = \
+            nn.Sequential(
+                *[MHSAB(chan) for _ in range(middle_blk_num)]
+            )
+
+        for num in dec_blk_nums:
+            self.ups.append(
+                nn.Sequential(
+                    nn.Conv2d(chan, chan * 2, 1, bias=False),
+                    nn.PixelShuffle(2)
+                )
+            )
+            chan = chan // 2
+            self.decoders.append(
+                nn.Sequential(
+                    *[DRBlock(chan, drdnum) for _ in range(num)]
+                )
+            )
+
+        self.padder_size = 2 ** len(self.encoders)
+
+    def forward(self, inp):
+        B, C, H, W = inp.shape
+        inp = self.check_image_size(inp)
+
+        x = self.intro(inp)
+
+        encs = []
+        for encoder, down in zip(self.encoders, self.downs):
+            x = encoder(x)
+            encs.append(x)
+            x = down(x)
+
+        x = self.middle_blks(x)
+
+        for decoder, up, enc_skip in zip(self.decoders, self.ups, encs[::-1]):
+            x = up(x)
+            x = x + enc_skip
+            x = decoder(x)
+
+        x = self.ending(x)
+        x = x + inp
+        x = x.view(x.size(0), -1)
+
+        return torch.squeeze(self.lin(x))
+
+    def check_image_size(self, x):
+        _, _, h, w = x.size()
+        mod_pad_h = (self.padder_size - h % self.padder_size) % self.padder_size
+        mod_pad_w = (self.padder_size - w % self.padder_size) % self.padder_size
+        x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h))
+        return x
+
+
+
+if __name__ == '__main__':
+
+    x = torch.rand([1,3,224,224])
+    net = SU_Net()
+    t = net(x)
+    print(t)
+    from torchstat import stat
+    stat(net, (3, 224, 224))
+    print("s")
